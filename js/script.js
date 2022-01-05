469e677e5989ad1c378a9b5e751e2142000ccad2
sarthakdhasmana <sarthakmay09@gmail.com>
2022-01-05 23:43:10 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-function hamburger() {
-    let hambtn = document.querySelector('.hamburger-btn');
-    console.log(hambtn);
-
-    // hambtn.append
+window.onload = function () {
+    let menu_btn = document.querySelector(".hamburger");
+    let mobile_menu = document.querySelector('.mobile-nav');
+    menu_btn.addEventListener("click", function () {
+        menu_btn.classList.toggle('is-active');
+        // mobile_menu.classList.toggle('is-active');
+    });
+    menu_btn.addEventListener('click', function () {
+        mobile_menu.classList.toggle('is-active');
+    });
 }
