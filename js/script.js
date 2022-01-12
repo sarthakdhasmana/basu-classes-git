85401ffc76b2c35c0e53a6f0ee1001b254813d2e
sarthakdhasmana <sarthakmay09@gmail.com>
2022-01-12 07:19:35 +0530

    bug fixes -textshadow&-arrowbtn

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 window.onload = function () {
     let menu_btn = document.querySelector(".hamburger");
     let mobile_menu = document.querySelector('.mobile-nav');
+    let banner = document.querySelector(".blog-section");
     menu_btn.addEventListener("click", function () {
         menu_btn.classList.toggle('is-active');
         // mobile_menu.classList.toggle('is-active');
+
+        //banner show none
+        banner.classList.toggle('hide');
     });
     menu_btn.addEventListener('click', function () {
         mobile_menu.classList.toggle('is-active');
